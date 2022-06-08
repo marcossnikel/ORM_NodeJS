a62cf037d56dc29acdb4ba8e3fd727816d5b440f
Marcos Nikel <marcosnikeldev@gmail.com>
2022-06-07 23:57:26 -0300

    apiConfigurations

--- api/index.js
+++ api/index.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 const app = express();
-app.use(bodyParser.json());
-
 const port = 3000;
+routes(app);
 
-
-app.get('/',(req,res)=>{
-    res.status(200)
-    .send({mensagem : 'testing api'});
-})
-
-
-app.listen(port,    () => console.log(`Api on em http://localhost${port}`));
+app.listen(port, () => console.log(`Api on em http://localhost${port}`));
 
 
 module.exports = app;
